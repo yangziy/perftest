d6014bac3a6323f2606ce3d2a2a7d4b1201092cb
Ziyang Yang <ziyang.yang@rice.edu>
2021-06-06 02:47:33 -0500

    run_server/run_client scripts

---
+++ scripts/run_client.sh
@@ -0,0 +1,16 @@
+set -x
+
+# ds01
+
+./ib_read_bw                 \
+--ib-dev=mlx5_1                     \
+             --CPU-freq             \
+             --duration=10          \
+             --port=10000           \
+             --size=2048            \
+             --post_list=32         \
+             --qp=5                 \
+             --mr_per_qp            \
+             10.0.3.9               
+
+set +x
---
+++ scripts/run_server.sh
@@ -0,0 +1,14 @@
+set -x
+
+# ds09
+
+./ib_read_bw                 \
+             --ib-dev=mlx5_2        \
+             --CPU-freq             \
+             --duration=10          \
+             --port=10000           \
+             --size=2048            \
+             --post_list=32         \
+             --mr_per_qp            \
+             --qp=5                 
+set +x
